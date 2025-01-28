1c425bdb36a2e590d3fa122254f8bb039f751470
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-01-29 01:31:30 +0530

    Stop Tracking venv

--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myBlog',
+    'DjangoBlog.myBlog',
     'user.apps.UserConfig',
     'crispy_forms',
     'crispy_tailwind',
